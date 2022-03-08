3c3d6b7c7bb62a884e7473c0e9232827a18f7a57
Erik Roberts <erik@bugg.co>
2022-03-08 11:41:53 -0500

    Add strict typing, code style enforcement, & modularize more of the code

---
+++ src/classes/Colors.ts
@@ -0,0 +1,8 @@
+const Colors: {[key: string]: [number, number, number]} = Object.freeze({
+    RED    : [ 255,   0,   0 ],
+    GREEN  : [ 0, 255,   0 ],
+    BLUE   : [ 0,   0, 255 ],
+    YELLOW : [ 255,  70,   0 ]
+});
+
+export default Colors;
